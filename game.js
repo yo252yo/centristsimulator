464b8bae17a33a4a8974b24f32236f167e97f04b
yo252yo <yo252yo@gmail.com>
2024-05-04 15:28:33 +0200

    basic support of action buttons

--- game.js
+++ game.js
@@ -52,3 +52,30 @@ let paused = false;
 function pause() {
     paused = !paused;
 }
+
+
+
+function choseAction(actionId) {
+    let action = document.getElementById(actionId);
+    s_points -= parseInt(action.dataset.cost);
+    s_points_per_year += parseInt(action.dataset.reward);
+
+}
+
+function addActionToList(actionId, cost, reward) {
+    let li = document.createElement("li");
+    li.id = actionId;
+    li.textContent = `${actionId}: -${cost}$, +${reward}$/s`;
+    li.dataset.cost = cost;
+    li.dataset.reward = reward;
+
+    li.addEventListener("click", function () {
+        choseAction(actionId);
+    });
+
+    document.getElementById("actions").appendChild(li);
+}
+
+addActionToList("action1", 1, 1);
+addActionToList("action2", 10, 10);
+addActionToList("action3", 100, 100);
