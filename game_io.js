a6dc3aebb353452c35a545a89cfa07e1e79bbd4c
yo252yo <yo252yo@gmail.com>
2024-05-09 00:14:40 +0200

    adds prettyprint for numbers

--- game_io.js
+++ game_io.js
@@ -1,15 +1,27 @@
 
 let start_year = (new Date()).getFullYear();
 
+function formatNumber(number) {
+    if (number >= 1000000000) {
+        return (number / 1000000000).toFixed(1) + 'B';
+    } else if (number >= 1000000) {
+        return (number / 1000000).toFixed(1) + 'M';
+    } else if (number >= 1000) {
+        return (number / 1000).toFixed(1) + 'K';
+    } else {
+        return number.toString();
+    }
+}
+
 function updateHtmlValues() {
     var year = start_year + Math.floor(seconds_elapsed / 4);
     var season = seconds_elapsed % 4;
     document.getElementById("date").innerHTML = `Year: ${year} ${"|".repeat(season)}${".".repeat(4 - season)}`;
 
-    document.getElementById("s_points").innerHTML = good_points;
-    document.getElementById("s_income").innerHTML = good_points_per_sec;
-    document.getElementById("c_points").innerHTML = BP;
-    document.getElementById("differencial").innerHTML = BP - good_points;
+    document.getElementById("s_points").innerHTML = formatNumber(good_points);
+    document.getElementById("s_income").innerHTML = formatNumber(good_points_per_sec);
+    document.getElementById("c_points").innerHTML = formatNumber(BP);
+    document.getElementById("differencial").innerHTML = formatNumber(BP - good_points);
 
     document.getElementById("population").innerHTML = `${population}`;
 
