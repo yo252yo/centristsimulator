e6a230e617e849dab588386a2e7c9628b62ac959
yo252yo <yo252yo@gmail.com>
2024-05-10 21:13:21 +0200

    import balance i did in google sheet

--- export.js
+++ export.js
@@ -11,6 +11,39 @@ function convertToCSV(obj) {
 const csvData = convertToCSV(TECHNOLOGIES);
 console.log(csvData);
 
+
+
+const csvData2 = `name,category,cost,income,level_cost_increase,level_income_increase,current_level
+wind turbines,1,5,1,1.1,0.98,0
+food waste ads,2,10,1,2,1.3,0
+carpooling ads,3,20,2,5,2,0
+LEDs,4,35,4,1.5,0.7,0
+silvopastures,5,50,6,7,1.2,0
+clean cooking,6,100,10,20,4,0
+improve insulation,4,500,50,1.5,0.9,0
+solar panels,1,1.0K,100,1.1,0.6,0
+veganism ads,2,1.5K,200,2,0.3,0
+electric cars,3,2.0K,300,5,0.1,0
+peatland protection,5,3.5K,400,7,1.3,0
+refrigerant,4,5.0K,500,1.5,1.49,0
+food waste subsidy,2,7.5K,2.0K,1.1,1.05,0
+more public transport,3,10.0K,6.0K,2.5,1.8,0
+Family planning,6,15.0K,7.5K,20,10,0
+metal recycling,4,20.0K,20.0K,1.3,1.05,0
+Perennial Staple Crops,5,25.0K,30.0K,7,2.6,0
+Plant based diet subsidy,2,30.0K,40.0K,2,1.2,0
+bioplastics,4,50.0K,50.0K,1.5,1.01,0
+Rnd for efficient vehicles,3,100.0K,10.0K,3,2.5,0
+Geothermal plant,1,150.0K,15.0K,1.1,1.05,0
+Food waste law,2,200.0K,22.0K,2,1.7,0
+Tropical Forest Restoration,5,250.0K,30.0K,7,4.5,0
+universal education,6,500.0K,75.0K,18,10,0
+Alternative cement,4,1.0M,250.0K,1.3,0.7,0
+Nuclear plant,1,5.0M,500.0K,1.6,0.6,0
+Plant based diet law,2,10.0M,750.0K,2.8,0.5,0
+Forbid monoculture,5,100.0M,1.0M,9,0.9,0
+5 minute city/bike infra,3,1000.0M,1.5M,2.5,2,0`;
+
 function convertToObject(csv) {
     const lines = csv.split('\n');
     const headers = lines[0].split(',');
@@ -27,5 +60,6 @@ function convertToObject(csv) {
     return result;
 }
 
-const importedObject = convertToObject(csvData);
+const importedObject = convertToObject(csvData2);
 console.log(importedObject);
+console.log(JSON.stringify(importedObject, null, 4));
--- game_actions.js
+++ game_actions.js
@@ -4,138 +4,267 @@ let LF_table = [1, 5, 10, 100, 1000, 50000, 1000000, 1000000000];
 let LICENSING_FEE = LF_table[LF_index];
 
 const TECHNOLOGIES = {
-    // ===========================================================
-    // ===========================================================
-    // TIER 1
-    "t1": {
-        name: "wind turbines",
-        category: 1,
-
-        cost: 5,
-        income: .5,
-        level_cost_increase: 1.1,
-        level_income_increase: 1.01,
-
-        current_level: 0,
+    "t1_1": {
+        "name": "wind turbines",
+        "category": 1,
+        "cost": 5,
+        "income": 1,
+        "level_cost_increase": 1.1,
+        "level_income_increase": 0.98,
+        "current_level": 0
     },
-    "t2": {
-        name: "food waste ads",
-        category: 2,
-
-        cost: 10,
-        income: 1,
-        level_cost_increase: 2.0,
-        level_income_increase: 1.4,
-
-        current_level: 0,
+    "t1_2": {
+        "name": "food waste ads",
+        "category": 2,
+        "cost": 10,
+        "income": 1,
+        "level_cost_increase": 2,
+        "level_income_increase": 1.3,
+        "current_level": 0
     },
-
-    "t3": {
-        name: "carpooling ads",
-        category: 3,
-
-        cost: 20,
-        income: 2,
-        level_cost_increase: 10,
-        level_income_increase: 2.5,
-
-        current_level: 0,
+    "t1_3": {
+        "name": "carpooling ads",
+        "category": 3,
+        "cost": 20,
+        "income": 2,
+        "level_cost_increase": 5,
+        "level_income_increase": 2,
+        "current_level": 0
     },
-    "t4": {
-        name: "LEDs",
-        category: 4,
-
-        cost: 35,
-        income: 4,
-        level_cost_increase: 1.5,
-        level_income_increase: 0.7,
-
-        current_level: 0,
+    "t1_4": {
+        "name": "LEDs",
+        "category": 4,
+        "cost": 35,
+        "income": 4,
+        "level_cost_increase": 1.5,
+        "level_income_increase": 0.7,
+        "current_level": 0
     },
-    "t5": {
-        name: "silvopastures",
-        category: 5,
-
-        cost: 50,
-        income: 6,
-        level_cost_increase: 7,
-        level_income_increase: 1.2,
-
-        current_level: 0,
+    "t1_5": {
+        "name": "silvopastures",
+        "category": 5,
+        "cost": 50,
+        "income": 6,
+        "level_cost_increase": 7,
+        "level_income_increase": 1.2,
+        "current_level": 0
     },
-    "t6": {
-        name: "clean cooking",
-        category: 6,
-
-        cost: 100,
-        income: 10,
-        level_cost_increase: 20,
-        level_income_increase: 5,
-
-        current_level: 0,
+    "t1_6": {
+        "name": "clean cooking",
+        "category": 6,
+        "cost": 100,
+        "income": 10,
+        "level_cost_increase": 20,
+        "level_income_increase": 4,
+        "current_level": 0
     },
-
-    // ===========================================================
-    // ===========================================================
-    // TIER 2
-    "u0": {
-        name: "improve insulation",
-        category: 4,
-
-        cost: 500,
-        income: 12,
-        level_cost_increase: 1.5,
-        level_income_increase: .7,//.9,
-
-        current_level: 0,
+    "t2_1": {
+        "name": "improve insulation",
+        "category": 4,
+        "cost": 500,
+        "income": 50,
+        "level_cost_increase": 1.5,
+        "level_income_increase": 0.9,
+        "current_level": 0
     },
-    "u1": {
-        name: "solar panels",
-        category: 1,
-
-        cost: 1000,
-        income: 20,
-        level_cost_increase: 1.1,
-        level_income_increase: 1.1,//1.2,
-
-        current_level: 0,
+    "t2_2": {
+        "name": "solar panels",
+        "category": 1,
+        "cost": 1000,
+        "income": 100,
+        "level_cost_increase": 1.1,
+        "level_income_increase": 0.6,
+        "current_level": 0
     },
-    "u2": {
-        name: "veganism ads",
-        category: 2,
-
-        cost: 1500,
-        income: 100,
-        level_cost_increase: 2.0,
-        level_income_increase: 1.8,//1.2,
-
-        current_level: 0,
+    "t2_3": {
+        "name": "veganism ads",
+        "category": 2,
+        "cost": 1500,
+        "income": 200,
+        "level_cost_increase": 2,
+        "level_income_increase": 0.3,
+        "current_level": 0
     },
-    "u3": {
-        name: "electric cars",
-        category: 3,
-
-        cost: 2000,
-        income: 300,
-        level_cost_increase: .6,
-        level_income_increase: .8,//.8,
-
-        current_level: 0,
+    "t2_4": {
+        "name": "electric cars",
+        "category": 3,
+        "cost": 2000,
+        "income": 300,
+        "level_cost_increase": 5,
+        "level_income_increase": 0.1,
+        "current_level": 0
     },
-    "u4": {
-        name: "peatland protection",
-        category: 5,
-
-        cost: 4000,
-        income: 500,
-        level_cost_increase: 7,
-        level_income_increase: 1.3,
-
-        current_level: 0,
+    "t2_5": {
+        "name": "peatland protection",
+        "category": 5,
+        "cost": 3500,
+        "income": 400,
+        "level_cost_increase": 7,
+        "level_income_increase": 1.3,
+        "current_level": 0
     },
-    // ===========================================================
-    // ===========================================================
-    // TIER 3
+    "t3_1": {
+        "name": "refrigerant",
+        "category": 4,
+        "cost": 5000,
+        "income": 500,
+        "level_cost_increase": 1.5,
+        "level_income_increase": 1.49,
+        "current_level": 0
+    },
+    "t3_2": {
+        "name": "food waste subsidy",
+        "category": 2,
+        "cost": 7500,
+        "income": 2000,
+        "level_cost_increase": 1.1,
+        "level_income_increase": 1.05,
+        "current_level": 0
+    },
+    "t3_3": {
+        "name": "more public transport",
+        "category": 3,
+        "cost": 10000,
+        "income": 6000,
+        "level_cost_increase": 2.5,
+        "level_income_increase": 1.8,
+        "current_level": 0
+    },
+    "t3_4": {
+        "name": "Family planning",
+        "category": 6,
+        "cost": 15000,
+        "income": 7500,
+        "level_cost_increase": 20,
+        "level_income_increase": 10,
+        "current_level": 0
+    },
+    "t3_5": {
+        "name": "metal recycling",
+        "category": 4,
+        "cost": 20000,
+        "income": 20000,
+        "level_cost_increase": 1.3,
+        "level_income_increase": 1.05,
+        "current_level": 0
+    },
+    "t3_6": {
+        "name": "Perennial Staple Crops",
+        "category": 5,
+        "cost": 25000,
+        "income": 30000,
+        "level_cost_increase": 7,
+        "level_income_increase": 2.6,
+        "current_level": 0
+    },
+    "t3_7": {
+        "name": "Plant based diet subsidy",
+        "category": 2,
+        "cost": 30000,
+        "income": 40000,
+        "level_cost_increase": 2,
+        "level_income_increase": 1.2,
+        "current_level": 0
+    },
+    "t3_8": {
+        "name": "bioplastics",
+        "category": 4,
+        "cost": 50000,
+        "income": 50000,
+        "level_cost_increase": 1.5,
+        "level_income_increase": 1.01,
+        "current_level": 0
+    },
+    "t4_1": {
+        "name": "Rnd for efficient vehicles",
+        "category": 3,
+        "cost": 100000,
+        "income": 10000,
+        "level_cost_increase": 3,
+        "level_income_increase": 2.5,
+        "current_level": 0
+    },
+    "t4_2": {
+        "name": "Geothermal plant",
+        "category": 1,
+        "cost": 150000,
+        "income": 15000,
+        "level_cost_increase": 1.1,
+        "level_income_increase": 1.05,
+        "current_level": 0
+    },
+    "t4_3": {
+        "name": "Food waste law",
+        "category": 2,
+        "cost": 200000,
+        "income": 22000,
+        "level_cost_increase": 2,
+        "level_income_increase": 1.7,
+        "current_level": 0
+    },
+    "t4_4": {
+        "name": "Tropical Forest Restoration",
+        "category": 5,
+        "cost": 250000,
+        "income": 30000,
+        "level_cost_increase": 7,
+        "level_income_increase": 4.5,
+        "current_level": 0
+    },
+    "t4_5": {
+        "name": "universal education",
+        "category": 6,
+        "cost": 500000,
+        "income": 75000,
+        "level_cost_increase": 18,
+        "level_income_increase": 10,
+        "current_level": 0
+    },
+    "t4_6": {
+        "name": "Alternative cement",
+        "category": 4,
+        "cost": 1000000,
+        "income": 250000,
+        "level_cost_increase": 1.3,
+        "level_income_increase": 0.7,
+        "current_level": 0
+    },
+    "t5_1": {
+        "name": "Nuclear plant",
+        "category": 1,
+        "cost": 5000000,
+        "income": 500000,
+        "level_cost_increase": 1.6,
+        "level_income_increase": 0.6,
+        "current_level": 0
+    },
+    "t5_2": {
+        "name": "Plant based diet law",
+        "category": 2,
+        "cost": 10000000,
+        "income": 750000,
+        "level_cost_increase": 2.8,
+        "level_income_increase": 0.5,
+        "current_level": 0
+    },
+    "t5_3": {
+        "name": "Forbid monoculture",
+        "category": 5,
+        "cost": 100000000,
+        "income": 1000000,
+        "level_cost_increase": 9,
+        "level_income_increase": 0.9,
+        "current_level": 0
+    },
+    "t5_4": {
+        "name": "5 minute city/bike infra",
+        "category": 3,
+        "cost": 1000000000,
+        "income": 1500000,
+        "level_cost_increase": 2.5,
+        "level_income_increase": 2,
+        "current_level": 0
+    }
 };
 
 function updateLicenseFee() {
@@ -210,7 +339,7 @@ function addToMarketplace(tech_id) {
 
     let li = document.createElement("li");
     li.id = "market_" + tech_id;
-    li.innerHTML = `${tech.name}: -${formatNumber(tech.cost)}$, +${formatNumber(tech.income)}$/s`;
+    li.innerHTML = `${tech.name}:<br /> -${formatNumber(tech.cost)}$, +${formatNumber(tech.income)}$/s`;
     li.dataset.cost = tech.cost;
     li.dataset.is_market = true;
     li.classList.add("li_" + tech.category);
