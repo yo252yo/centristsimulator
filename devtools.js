49832c8c4d10083094a1b0875840c6d78f52f3e7
yo252yo <yo252yo@gmail.com>
2024-05-10 13:28:29 +0200

    explore exhaustively the space of all strats

--- devtools.js
+++ devtools.js
@@ -1,4 +1,6 @@
 
+GAME_PAUSED = true;
+
 let expected_cash = [];
 let milestone = {
     0: {
@@ -31,13 +33,12 @@ let milestone = {
     },
 };
 
+
 function r() {
     window.location.href = window.location.href;
 }
 
 function evalTech(tech_id, start) {
-    GAME_PAUSED = true;
-
     start = start || 0;
     let tech = TECHNOLOGIES[tech_id];
     if (!start in milestone) {
@@ -62,8 +63,12 @@ function evalTech(tech_id, start) {
 }
 
 
-function bestrun(start, start_cash, start_income, start_local_lfindex) {
-    GAME_PAUSED = true;
+function bestrun(start, start_cash, start_income, start_local_lfindex, start_techtree) {
+    var tech_tree = start_techtree || Object.assign({}, TECHNOLOGIES);
+    for (var t in tech_tree) {
+        tech_tree[t] = Object.assign({}, tech_tree[t]);
+    }
+
     var start = start || 0;
     let cash = start_cash || milestone[start].cash;
     let income = start_income || milestone[start].income;
@@ -73,54 +78,59 @@ function bestrun(start, start_cash, start_income, start_local_lfindex) {
     for (var i = start; i < 91; i++) {
         cash += income;
 
-        var best = 0;
-        var argbest = "";
-        var bestnow = 0;
-        for (var t in TECHNOLOGIES) {
-            var tech = TECHNOLOGIES[t];
-            var license = 0;
-            if (tech.current_level == 0) {
-                license += license_price;
-            }
-            var cost = (tech.cost + license);
-            if (cost < cash) {
-                if (tech.income > bestnow) {
-                    bestnow = tech.income;
+        purchased = true;
+        while (purchased) {
+            var best = 0;
+            var argbest = "";
+            var bestnow = 0;
+            for (var t in tech_tree) {
+                var tech = tech_tree[t];
+                var license = 0;
+                if (tech.current_level == 0) {
+                    license += license_price;
+                }
+                var cost = (tech.cost + license);
+                if (cost < cash) {
+                    if (tech.income > bestnow) {
+                        bestnow = tech.income;
+                    }
                 }
             }
-        }
 
-        for (var t in TECHNOLOGIES) {
-            var tech = TECHNOLOGIES[t];
-            var license = 0;
-            if (tech.current_level == 0) {
-                license += license_price;
+            for (var t in tech_tree) {
+                var tech = tech_tree[t];
+                var license = 0;
+                if (tech.current_level == 0) {
+                    license += license_price;
+                }
+                var cost = (tech.cost + license);
+                if (cost > cash) {
+                    // add the miss from waiting
+                    var wait = (cost - cash) / income;
+                    cost += bestnow * wait;
+                }
+                var gain = tech.income / cost;
+
+                if (gain > best) {
+                    best = gain;
+                    argbest = tech;
+                }
             }
-            var cost = (tech.cost + license);
-            if (cost > cash) {
-                // add the miss from waiting
-                var wait = (cost - cash) / income;
-                cost += bestnow * wait;
-            }
-            var gain = tech.income / cost;
 
-            if (gain > best) {
-                best = gain;
-                argbest = tech;
-            }
-        }
+            purchased = false;
+            if (cash > argbest.cost) {
+                purchased = true;
+                console.log(".    PURCHASE: " + argbest.name + argbest.current_level);
+                cash -= argbest.cost;
+                income += argbest.income;
+                argbest.cost *= argbest.level_cost_increase;
+                argbest.income *= argbest.level_income_increase;
+                argbest.current_level += 1;
 
-        if (cash > argbest.cost) {
-            console.log(".    PURCHASE: " + argbest.name + argbest.current_level);
-            cash -= argbest.cost;
-            income += argbest.income;
-            argbest.cost *= argbest.level_cost_increase;
-            argbest.income *= argbest.level_income_increase;
-            argbest.current_level += 1;
-
-            if (argbest.current_level == 1) {
-                local_lfindex++;
-                license_price = LF_table[local_lfindex];
+                if (argbest.current_level == 1) {
+                    local_lfindex++;
+                    license_price = LF_table[local_lfindex];
+                }
             }
         }
         if ([10, 30, 60, 90, 120, 240, 300].includes(i)) {
@@ -130,10 +140,11 @@ function bestrun(start, start_cash, start_income, start_local_lfindex) {
 }
 
 function best_run_with(tech_id) {
-
-    GAME_PAUSED = true;
-
-    let tech = TECHNOLOGIES[tech_id];
+    var tech_tree = Object.assign({}, TECHNOLOGIES);
+    for (var t in tech_tree) {
+        tech_tree[t] = Object.assign({}, tech_tree[t]);
+    }
+    let tech = tech_tree[tech_id];
     let cash = milestone[0].cash;
     let income = milestone[0].income;
 
@@ -144,8 +155,86 @@ function best_run_with(tech_id) {
             income += tech.income;
             tech.cost *= tech.level_cost_increase;
             tech.income *= tech.level_income_increase;
-            bestrun(i, cash, income, 1);
+            tech.current_level += 1;
+            bestrun(i, cash, income, 1, tech_tree);
             return;
         }
     }
 }
+
+
+
+
+
+function try_strat(strat, timestamp_end) {
+    var costs = [];
+    var incomes = [];
+    var levels = [];
+    if (costs.length == 0) { // populate from tech tree
+        var techs = Object.values(TECHNOLOGIES);
+        for (let i in techs) {
+            costs[i] = techs[i].cost;
+            incomes[i] = techs[i].income;
+            levels[i] = 0;
+        }
+    }
+
+    var cash = 1;
+    var income = 10;
+    var current_license = 0;
+
+    var strat_index = 0;
+
+    for (var t = 0; t <= timestamp_end; t++) {
+        cash += income;
+        if (strat_index > strat.length - 1) {
+            throw "Insufficient strat;"
+        }
+
+        var next = strat[strat_index];
+
+        var purchased = true;
+        var incremented = false;
+        while (purchased) {
+            purchased = false;
+
+            var cost = costs[next];
+            if (levels[next] == 0) {
+                cost += LF_table[current_license];
+            }
+            if (cost <= cash) {
+                if (levels[next] == 0) {
+                    current_license++;
+                }
+                cash -= cost;
+                income += incomes[next];
+                incomes[next] *= techs[next].level_income_increase;
+                costs[next] *= techs[next].level_cost_increase;
+                purchased = true;
+
+                if (!incremented) {
+                    incremented = true;
+                    strat_index++;
+                }
+            }
+        }
+    }
+
+    return income;
+}
+
+var ALL_STRATS = {};
+function try_all_strat(prefix) {
+    var strat = prefix || "";
+    for (var s = 0; s <= 6; s++) {
+        try {
+            var r = try_strat(strat + s, 60);
+            console.log((strat + s) + ": " + r);
+            ALL_STRATS[(strat + s)] = r;
+        } catch (e) {
+            try_all_strat(strat + s);
+        }
+    }
+}
+try_all_strat();
+console.log(ALL_STRATS);
