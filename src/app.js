3759ec364dd8e57e09cd3ee76d5fd55c235a5866
yo252yo <yo252yo@gmail.com>
2024-05-29 15:05:56 +0200

    structure for SFX with volume check in options

---
+++ src/app.js
@@ -0,0 +1,11 @@
+
+function sfx(name) {
+    var audio = new Audio();
+    audio.src = `../files/sfx/${name}.ogg`;
+
+    const savedSFXVolume = localStorage.getItem('sfx_volume') || 100;
+    if (savedSFXVolume !== null) {
+        audio.volume = savedSFXVolume / 100;
+    }
+    audio.play();
+}
